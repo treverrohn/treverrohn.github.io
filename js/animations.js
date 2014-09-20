a10bf97f72e4a37607523b6c5a0be65ff81da5aa
Trever Rohn <trohn12@gmail.com>
2014-09-20 01:41:07 -0500

    Initial commit

---
+++ js/animations.js
@@ -0,0 +1,25 @@
+$(document).ready(bindEvents);
+
+function bindEvents() {
+	$("#flash").click(flashTrever);
+	$("#slide").click(slideTrever);
+	$("#color").click(colorTrever);
+}
+
+function flashTrever() {
+	$("#trever").stop().fadeToggle("fast")
+		.fadeToggle("fast")
+		.fadeToggle("fast")
+		.fadeToggle("fast");
+}
+
+function slideTrever() {
+	$("#trever").stop().animate({"top": "100px"})
+		.animate({"top": "0px"});
+}
+
+function colorTrever() {
+	$("#img_color").css({backgroundColor: "rgb("+(Math.floor(Math.random()*256))+","+(Math.floor(Math.random()*256))+","+(Math.floor(Math.random()*256))+")"});
+	$("#img_color").stop().animate({"opacity": "0.3"},500)
+		.animate({"opacity": "0"},500);
+}
